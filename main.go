12ef8254ea90b225fa09d0c284103aab0d02eedd
Tyrion Lannister <beerandncoding@gmail.com>
2018-07-02 01:44:54 +0530

    Clean report card

--- main.go
+++ main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"log"
-    "net/http"
-	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
-    "github.com/multicloudsolutions/cloudy/modules"
+        "log"
+        "net/http"
+        "github.com/go-sql-driver/mysql"
+        "database/sql"
+        "github.com/multicloudsolutions/cloudy/modules"
 
 )
 
 func main() {
-	db, err := sql.Open("mysql", "test:test@/test?charset=utf8")
-	if err != nil {
-		log.Fatal(err)
-	}
-	a := ac.AccountHandler{Db: db}
-    http.Handle("/account", &a)
-    http.ListenAndServe(":8080", nil)
+        db, err := sql.Open("mysql", "test:test@/test?charset=utf8")
+        if err != nil {
+                log.Fatal(err)
+        }
+        a := ac.AccountHandler{Db: db}
+        http.Handle("/account", &a)
+        http.ListenAndServe(":8080", nil)
 }
--- modules/account.go
+++ modules/account.go
@@ -1,40 +1,41 @@
 package ac
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"database/sql"
 )
 
+// AccountHandler is the db connection
 type AccountHandler struct {
-    Db *sql.DB
+	Db *sql.DB
 }
 
 func (h *AccountHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    var account_type, access_key, secret_key string
-    var id int64
-    var err error
+	var accountType, accessKey, secretKey string
+	var id int64
+	var err error
 
-    log.Print("Adding new account")
+	log.Print("Adding new account")
 
-    account_type = r.URL.Query().Get("account_type")
-    access_key = r.URL.Query().Get("access_key")
-    secret_key = r.URL.Query().Get("secret_key")
+	accountType = r.URL.Query().Get("account_type")
+	accessKey = r.URL.Query().Get("access_key")
+	secretKey = r.URL.Query().Get("secret_key")
 
-    stmt, err := h.Db.Prepare("INSERT test SET account_type=?,access_key=?,secret_key=?")
-    checkErr(err)
+	stmt, err := h.Db.Prepare("INSERT test SET account_type=?,access_key=?,secret_key=?")
+	checkErr(err)
 
-    res, err := stmt.Exec(account_type, access_key, secret_key)
-    checkErr(err)
-    id, err = res.LastInsertId()
-    checkErr(err)
+	res, err := stmt.Exec(accountType, accessKey, secretKey)
+	checkErr(err)
+	id, err = res.LastInsertId()
+	checkErr(err)
 
-    fmt.Fprintf(w, "Account added successfully, id: %d", id)
+	fmt.Fprintf(w, "Account added successfully, id: %d", id)
 }
 
 func checkErr(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
